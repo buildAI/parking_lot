069012bd4ee63a83881943cc995d18def8049dfe
Karthik Yelchuru <yelkarthik@gmail.com>
2019-10-07 11:56:37 +0530

    Integerify column, row in slot if it's string

--- lib/slot.rb
+++ lib/slot.rb
@@ -2,6 +2,8 @@ class Slot
   attr_reader :column, :row
 
   def initialize(column, row)
+    column = column.to_i if column&.class == String
+    row =  row.to_i if row&.class == String
     raise ArgumentError, "Invalid row/column value be less than zero"  if (column < 1 || row < 1)
     @column = column
     @row = row
